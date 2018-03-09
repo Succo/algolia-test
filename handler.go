b7dfb1981142b61d9d7731504baca74a6ef916b7
Fabrice <fabrice.vaillant@centraliens.net>
2018-03-09 10:31:42 +0100

    Returns a proper response for the count route

--- handler.go
+++ handler.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+type countResponse struct {
+	Count int `json:count`
+}
+
 func (I *Index) queryCounter(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	date := params["date"]
-	fmt.Fprint(w, len(I.getRange(date)))
+	resp := countResponse{Count: len(I.getRange(date))}
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	err := json.NewEncoder(w).Encode(resp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
 }
 
 func (I *Index) queryPopularity(w http.ResponseWriter, r *http.Request) {
